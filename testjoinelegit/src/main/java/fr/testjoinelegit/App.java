e481e9133a9a9f11fbb8319e8243f2b1b306e113
Joinel CHELEUX <lainy06@hotmail.fr>
2017-12-12 15:16:04 +0100

    Premier test avec Git

---
+++ testjoinelegit/src/main/java/fr/testjoinelegit/App.java
@@ -0,0 +1,13 @@
+package fr.testjoinelegit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
